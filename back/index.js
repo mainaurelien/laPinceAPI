935da13fb85cc4aeafcdca0ace3d98d23967aa22
admin <admin@admins-MBP.lan>
2025-06-10 19:06:21 +0200

    docker: add backend in back/ + docker-compose config

--- back/index.js
+++ back/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import helmet from "helmet";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(helmet());
